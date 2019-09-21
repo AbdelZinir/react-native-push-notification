b0c161e1d59572d50c114a468c1045b6716440de
Abdellah INIRZ <abdellah.inirz@gmail.com>
2019-09-21 10:52:43 +0100

    Open my application on schedulle notification received

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationPublisher.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationPublisher.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import static com.dieam.reactnativepushnotification.modules.RNPushNotification.LOG_TAG;
